99f87dba50dea67d95d0722bc4c83e52bb3928dc
Senidu Ravihara <senidumee16@gmail.com>
2025-12-11 12:54:05 +0530

    sitemap update

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: ['/admin/', '/student/'],
     },
-    sitemap: 'https://nadeeteacher.com/sitemap.xml',
+    sitemap: 'https://nadeeteacher.online/sitemap.xml',
   }
 }
